b407ecd41aa26dca8b6a28be63af4415376ce888
Michal Siek <michal.siek21@gmail.com>
2023-07-19 13:07:27 +0200

    added coe + extensions tests passed (I think, some wierd tests occured)

--- src/extension.js
+++ src/extension.js
@@ -19,10 +19,10 @@ const book = {
 const isbn13 = '978-0132350884'
 
 // 1. Set this to the book name - using the book object
-const name = ''
+const name = book.name
 
 // 2. Set this to the isbn 10 value - using the book object
-const isbn10 = ''
+const isbn10 = book.isbn.isbn10
 
 // Do not modify this basket object directly
 const basket = {
@@ -42,10 +42,10 @@ const basket = {
 }
 
 // 3. Set this variable to the length of the baskets voucher codes array - using the basket object
-const numberOfVoucherCodes = null
+const numberOfVoucherCodes = basket.voucherCodes.length
 
 // 4. Set this variable to the first element in of the baskets voucher codes array - using the basket object
-const firstVoucherCode = null
+const firstVoucherCode = basket.voucherCodes[0]
 
 // Do not edit this exported object
 module.exports = {
